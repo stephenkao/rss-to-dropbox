f5edf31f6389f08a97834638d91393f8c91b1a80
Stephen Kao <stephen.yuchen.kao@gmail.com>
2021-11-21 13:47:30 -0500

    small fixes

--- rss_to_dropbox.rb
+++ rss_to_dropbox.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 
 # Usage:
-# ruby rss_to_dropbox.rb <rss_url>
+# ruby rss_to_dropbox.rb <rss_url> [<start_idx>] [<end_idx>]
+# TODO:
+#   - start, end idx args
+#   - total count arg
 
 require 'dotenv/load'
 
@@ -10,6 +13,7 @@ require 'dropbox_api'
 require 'fileutils'
 require 'nokogiri'
 require 'open-uri'
+require 'ruby-progressbar'
 
 require 'pry'
 
@@ -27,9 +31,14 @@ throw 'No RSS URL was provided' if !rss_url
 doc = Nokogiri::XML(URI.open(rss_url))
 
 podcast_title = sanitize_filename(doc.css('rss > channel > title').inner_text)
-podcast_episodes = doc.css('rss channel item')
+podcast_episodes = doc.css('rss channel item').reverse # RSS in descending order
+start_idx = (ARGV[1] || 0).to_i
+end_idx = (ARGV[2] || podcast_episodes.count - 1).to_i
 
-puts "Found #{podcast_episodes.count} episodes for podcast #{podcast_title}"
+puts ''
+puts "Found #{podcast_episodes.count} total episodes for podcast #{podcast_title}"
+puts "Transferring episodes between #{start_idx} and #{end_idx}"
+puts ''
 
 # initialize Dropbox
 access_token = ENV['DROPBOX_ACCESS_TOKEN']
@@ -51,23 +60,48 @@ FileUtils.mkdir_p(temp_folder_path)
 folder_data = client.list_folder(folder_path)
 title_hash = folder_data.entries.map { |i| [i.name, true] }.to_h
 
-podcast_episodes.each_with_index do |episode, idx|
-  # TODO: assuming .mp3 here
-  #filename = "#{sanitize_filename(episode.css('title').inner_text)}.mp3"
+skipped = []
 
-  # download episode to local, upload to Dropbox, delete from local
-  url = episode.css('enclosure').attribute('url').value
-  # TODO: stream directly via https://github.com/janko/down/issues/18#issuecomment-371051686
-  tempfile = Down.download(url)
-  client.upload_by_chunks(
-    # "#{folder_path}/#{tempfile.original_filename}",
-    "#{folder_path}/#{idx+1}.mp3",
-     tempfile,
-     chunk_size:  100 * 1024 * 1024
-  )
-  tempfile.close
+podcast_episodes[start_idx..end_idx].each_with_index do |episode, idx|
+  begin
+    # TODO: assuming .mp3 here
+    #filename = "#{sanitize_filename(episode.css('title').inner_text)}.mp3"
 
-  puts "🎉🎉🎉 Uploaded to #{folder_path}/#{tempfile.original_filename}...sleeping before continuing"
-  sleep 10
-  puts ''
+    url = episode.css('enclosure').attribute('url').value
+    # TODO: stream directly via https://github.com/janko/down/issues/18#issuecomment-371051686
+
+    total_size = 0
+    progressbar = ProgressBar.create(title: 'Progress')
+    tempfile = Down.download(
+      url,
+      content_length_proc: -> (content_length) { total_size = content_length },
+      progress_proc: lambda do |progress|
+        break if progress == total_size
+
+        progressbar.progress = (progress.to_f / total_size) * 100
+      end
+    )
+
+    # TODO: better file naming
+    # filename = "#{folder_path}/#{idx+1}.mp3",
+    filename = "#{folder_path}/#{tempfile.original_filename}"
+
+    client.upload_by_chunks(
+      filename,
+      tempfile,
+      chunk_size:  100 * 1024 * 1024
+    )
+    tempfile.close
+
+    puts ''
+    puts "🎉🎉🎉 Uploaded to #{folder_path}/#{filename}"
+    puts ''
+  rescue StandardError => e
+    puts "⚠️ Could not save episode #{idx}: #{e}"
+    skipped.push(idx);
+  end
+end
+
+if !skipped.empty?
+  puts "Skipped ${skipped} -- try redownloading some other time, ya dumb!"
 end
